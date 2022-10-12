b2d8490f40cf7b8b1b2132f96ec8d0c7e53a145d
ITX-AK-SPIDER <114110712+ITX-AK-SPIDER@users.noreply.github.com>
2022-10-12 15:39:06 +0500

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,15 +1,14 @@
 import os
 os.system('clear')
 print("[×] Hello FB KID how Are You")
-print('[×] Aak Bat To Pata Chale Tu Bap Ke Olad Nhi Ha 🤣 ')
-
 print('\n[1] Start Fuck FB-KID\n[0] Exit')
 x=input('[•] Option: ')
 if x in ['1','01']:
-  xx="Fuck you fb kid,  Team AK-SPIDER"
-  open('/data/data/com.termux/files/usr/bin/.88888Pro777phpo-cov','w').write(xx)
-  print('[√] Bypass done...\n[•] Enjoy And Fuck FB KID')
-  
+    xx="Fuck you fb kid,  Team AK-SPIDER"
+    open('/data/data/com.termux/files/usr/bin/.88888Pro777phpo-cov','w').write(xx)
+    print('[√] Bypass done...\n[•] Enjoy And Fuck FB KID')
+    import pro
+    pro.main1()
 else:
-  exit('[•] See U Again 🙂❤ ')
+    exit('[•] See U Again 🙂❤ ')
   
