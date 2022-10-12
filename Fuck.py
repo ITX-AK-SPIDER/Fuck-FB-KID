8dc12bff4826770eec45ac1cbbc1cc47f1376048
ITX-AK-SPIDER <114110712+ITX-AK-SPIDER@users.noreply.github.com>
2022-10-12 15:37:43 +0500

    Create Fuck.py

---
+++ Fuck.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+print("[×] Hello FB KID how Are You")
+print('[×] Aak Bat To Pata Chale Tu Bap Ke Olad Nhi Ha 🤣 ')
+
+print('\n[1] Start Fuck FB-KID\n[0] Exit')
+x=input('[•] Option: ')
+if x in ['1','01']:
+  xx="Fuck you fb kid,  Team AK-SPIDER"
+  open('/data/data/com.termux/files/usr/bin/.88888Pro777phpo-cov','w').write(xx)
+  print('[√] Bypass done...\n[•] Enjoy And Fuck FB KID')
+  
+else:
+  exit('[•] See U Again 🙂❤ ')
+  
