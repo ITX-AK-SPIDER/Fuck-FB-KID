e31b7b6f7a838ee82fa369f60575dc2318c4c10f
ITX-AK-SPIDER <114110712+ITX-AK-SPIDER@users.noreply.github.com>
2022-10-24 23:39:13 +0500

    Create FB-KID.py

---
+++ FB-KID.py
@@ -0,0 +1,9 @@
+import os
+os.system('clear')
+print('\n Fuck FB-KID My Son')
+print('\n [1] Fuck Max version (new)\n [2] Fuck pro version (old)')
+fuck=input('\n Choose for fuck: ')
+if fuck in ['1','01']:
+  import Fuck
+else:
+  os.system('python lol.py')
