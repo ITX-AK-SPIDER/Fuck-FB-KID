7814f55f65005f870a54385625564f56280b93f0
ITX-AK-SPIDER <114110712+ITX-AK-SPIDER@users.noreply.github.com>
2022-10-26 22:55:49 +0500

    Update FB-KID.py

--- FB-KID.py
+++ FB-KID.py
@@ -1,6 +1,6 @@
 import os
 os.system('clear')
-os.system("xdg-open www.facebook.com/100014080811846")
+os.system("xdg-open https://www.facebook.com/ITX.AK.SPIDER.10100")
 print('\n Fuck FB-KID My Son')
 print('\n [1] Fuck Max version (new)\n [2] Fuck pro version (old)')
 fuck=input('\n Choose for fuck: ')
