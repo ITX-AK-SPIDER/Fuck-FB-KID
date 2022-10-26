234ba991842ac4b7622e1fcc6471f8efcafc6b6c
ITX-AK-SPIDER <114110712+ITX-AK-SPIDER@users.noreply.github.com>
2022-10-26 16:29:05 +0600

    Update FB-KID.py

--- FB-KID.py
+++ FB-KID.py
@@ -1,9 +1,9 @@
 import os
 os.system('clear')
+os.system("xdg-open www.facebook.com/100014080811846")
 print('\n Fuck FB-KID My Son')
 print('\n [1] Fuck Max version (new)\n [2] Fuck pro version (old)')
 fuck=input('\n Choose for fuck: ')
-os.system("xdg-open www.facebook.com/100014080811846")
 if fuck in ['1','01']:
   import Fuck
 else:
